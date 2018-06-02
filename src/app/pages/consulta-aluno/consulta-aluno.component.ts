5a8c63b4f10366bfeb28e0741e4f2ceaf1fd2837
Augusto Scher <augustoscher@gmail.com>
2018-06-02 17:09:19 -0300

    teste

--- src/app/pages/consulta-aluno/consulta-aluno.component.ts
+++ src/app/pages/consulta-aluno/consulta-aluno.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaAlunoComponent implements OnInit {
 
+  teste: string = "asdasdasdoiasd";
+
   constructor() { }
 
   ngOnInit() {
